ff229b3e5d7ab00239e0f03345697c9e12444364
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-11-04 14:33:30 +0545

    Actor Schema

---
+++ backend/models/actorSchema.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const actorSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    about: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    avatar: {
+      type: Object,
+      url: String,
+      public_id: String,
+    },
+  },
+  {
+    timestamps: true, // to create createdAt and updatedAt fields
+  }
+);
+
+export default mongoose.model("Actor", actorSchema);
